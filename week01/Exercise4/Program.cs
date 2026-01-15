88c93ede74d59eeb639d8b7e26d3ea62e210fa8e
btanatswa <128266721+webcraftingtwo@users.noreply.github.com>
2026-01-15 23:14:01 +0200

    Enhance Program.cs with statistics calculations
    Added functionality to compute the average, find the maximum and minimum positive numbers, and sort the list of integers.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,4 +1,4 @@
-using System;
+using System; 
 using System.Collections.Generic;
 
 class Program
@@ -33,7 +33,49 @@ class Program
         Console.WriteLine($"The sum is: {sum}");
 
         // Part 2: Compute the average
-        // Notice that we first cast the sum variable to be a float. Otherwise, because
+        // By making one of the variables a float first, the computer knows that it has to
+        // do the floating point division, and we get the decimal value that we expect.
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+
+        // Stretch Challenge 1: Find the smallest positive number
+        // We initialize this to a very large number so that the first positive number
+        // we find will definitely be smaller than it.
+        int minPositive = int.MaxValue;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < minPositive)
+            {
+                minPositive = number;
+            }
+        }
+        
+        Console.WriteLine($"The smallest positive number is: {minPositive}");
+
+        // Stretch Challenge 2: Sort the list
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
         // both the sum and the count are integers, the computer will do integer division
         // and I will not get a decimal value (even though it puts the result into a float variable).
 
