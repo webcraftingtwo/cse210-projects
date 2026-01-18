2496ed4e5917452d5d328bead1f6e3eb84f9e693
btanatswa <128266721+webcraftingtwo@users.noreply.github.com>
2026-01-18 15:41:39 +0200

    Resume app first enhancement

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Resume 
+{
+  public string _name;
+  // initialize list to a new list
+  public List<Job> _Jobs = new List<Job>();
+  public void Display()
+  {
+    console.WriteLine($"Name: {_name}");
+    console.WriteLine("Jobs: "
+
+    foreach(Job job in _jobs) 
+        {
+            job.Display();
+        }
+  }
+}
