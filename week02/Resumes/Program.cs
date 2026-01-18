6c0d070379155e66c0974be95c31a96f7649906b
btanatswa <128266721+webcraftingtwo@users.noreply.github.com>
2026-01-18 16:06:25 +0200

    Add job entries and display resume in Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -3,7 +3,25 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+    {   
+        Job job1 = new Job();
+        job1._jobTitle = "Algorithmic Trader";
+        job1._company = "ChaosEngines";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Junior Software Engineer";
+        job2._company = "MoCapital";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Blessed Mbewe";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
