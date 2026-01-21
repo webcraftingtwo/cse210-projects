67dc861d063fad7efb73c1fed17c6b4e1dae0dea
btanatswa <128266721+webcraftingtwo@users.noreply.github.com>
2026-01-21 07:30:45 +0200

    Add PromptGenerator class for daily journaling prompts

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public PromptGenerator()
+    {
+        // Initialize the list with the required prompts + some custom ones
+        _prompts.Add("Who was the most interesting person I interacted with today?");
+        _prompts.Add("What was the best part of my day?");
+        _prompts.Add("How did I see the hand of the Lord in my life today?");
+        _prompts.Add("What was the strongest emotion I felt today?");
+        _prompts.Add("If I had one thing I could do over today, what would it be?");
+        _prompts.Add("What is one thing I am grateful for today?");
+        _prompts.Add("What was the biggest challenge I faced today?");
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
+
