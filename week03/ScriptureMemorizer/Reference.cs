9d58a14d3bcf0bda068214d22acf28521c9be404
webcraftingtwo <mbewefx17@gmail.com>
2026-02-01 22:14:10 +0200

    updates to w3 and w4

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,33 @@
+Using System;
+
+public class Reference {
+
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse) {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endverse;
+    }
+    
+    public Reference(string book, int chapter, int verse, int endverse) {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endverse;
+    }
+    
+    public string GetDisplayText() {
+        if(_verse==_endVerse) {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else{
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Word {
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text) {
+        _text  = text;
+        _isHidden = false; // word is visible by default
+    }
+    public void Hide() {
+        _isHidden = true;
+    }
+    
+    public void Show() {
+        _isHidden = false;
+    }
+
+    public void isHidden() {
+        return _isHidden;
+    }
+    public string GetDisplayText() {
+        if(isHidden == false) {
+            return _text;
+        }
+        else {
+            string _underscores = "";
+            foreach(char letter in _text) {
+                _underscores+="_";
+            }
+            return _underscores;
+        }
+    }
+}
