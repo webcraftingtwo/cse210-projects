adf1f65dff91fa9a6d522548393cc7f58e9f0565
btanatswa <128266721+webcraftingtwo@users.noreply.github.com>
2026-02-18 07:18:14 +0200

    Update Goal.cs

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,1 +1,37 @@
+using System;
 
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    
+    public virtual string GetDetailsString()
+    {
+        string status = IsComplete() ? "[X]" : "[ ]";
+        return $"{status} {_shortName} ({_description})";
+    }
+
+    public abstract string GetStringRepresentation();
+
+    // Getter for points to help with score calculation
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public string GetName()
+    {
+        return _shortName;
+    }
+}
